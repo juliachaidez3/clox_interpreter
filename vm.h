e60686dbc2d5e38ce3bc15f0a9bc8232b50cec64
juliachaidez3 <jmchaidez@cpp.edu>
2026-03-24 22:52:49 -0700

    completed challenge 15.3

--- vm.h
+++ vm.h
@@ -4,13 +4,12 @@
 #include "chunk.h"
 #include "value.h"
 
-#define STACK_MAX 256
-
 typedef struct {
     Chunk* chunk;
     uint8_t* ip;
-    Value stack[STACK_MAX];
+    Value* stack;
     Value* stackTop;
+    int stackCapacity;
 } VM;
 
 typedef enum {
